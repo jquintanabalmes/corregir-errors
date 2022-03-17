6fc94f065c6c143a429b5160f3022ba083b49b6c
Levon <lgukasya20@jaumebalmes.net>
2022-03-17 19:39:22 +0100

    fix bugs alumne 8 i 9

--- arrays_javascript.js
+++ arrays_javascript.js
@@ -33,10 +33,10 @@ primer = fruites.shift();  //alumne7 // Elimina "Maduixa" de l'inicio
 fruites.push('Maduixa');
 
 //Trobar l'índex d'un element de l'Array
-let pos = fruites.indexof('Taronja');  //alumne8 // pos és la posició
+let pos = fruites.indexOf('Taronja');  //alumne8 // pos és la posició
 
 //Eliminar un únic element mitjançant la seva posició
-let elementEliminat = frutes.splice(pos, 1);  //alumne9 // no hi ha errors
+let elementEliminat = frutes.splice(pos, 1);  //alumne9
 
 //Eliminar varis elements a partir d'una posició
 let vegetals = ['Carbassó', 'Esbergínia', 'Rave', 'Pastanaga'];  //alumne10
